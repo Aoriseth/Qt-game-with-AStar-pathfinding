44bf8ae759e3de8b0b976146756668c8cf50d5db
r0481919 <r0481919@01ee0038-99c3-4e28-84e8-787d8bde98dd>
2016-11-17 20:59:10 +0000

    Visualize my world: display every tile as a square of a grey value relative to its value. Visualize healthpacks:display as image with the position relative to it's value.

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -9,12 +9,5 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-    World *myWorld = new World();//(100,100,QImage(":/new/pictures/resources/worldmap4.png"));
-    std::vector<std::unique_ptr<Tile>> tiles = myWorld->createWorld(":/new/pictures/resources/worldmap4.png");
-    std::vector<std::unique_ptr<Enemy>> enemys = myWorld->getEnemies(10);
-    std::vector<std::unique_ptr<Tile>> healthpacks = myWorld->getHealthPacks(6);
-    std::unique_ptr<Protagonist> pro = myWorld->getProtagonist();
-    //w.
-
     return a.exec();
 }
